515da7c5269d6efefadc51055b86741f6ef4ce8c
Stypox <stypox@pm.me>
2020-04-21 23:58:51 +0200

    Remove wildcard import

--- codelabs_extractor/codelab_extractor.py
+++ codelabs_extractor/codelab_extractor.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup as Html
 from bs4.element import NavigableString
-from .utils import *
+from .utils import getPageHtml, firstMatchRegex, optionalGet
 from .elements import *
 import re
 import os
